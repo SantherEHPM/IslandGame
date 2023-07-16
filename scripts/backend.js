17e3953a3a18785fef95cff1cb1d9946fce354c0
Santher <erickhernandop@gmail.com>
2023-07-16 10:09:43 -0500

    v2.0
    Lectura de json \n Plantilla terminada \n Navegacion configurada:

--- scripts/backend.js
+++ scripts/backend.js
@@ -1,55 +1,18 @@
-/*class Datos{
-    constructor(){
-        this.data = []
-        this.getApi()
-    }
 
-    getData(){
-        return this.data
-        /*setTimeout(()=>{
-        if(this.data===undefined){
-            this.getData()
-        }
-        else{
-            data = this.data
-            return this.data
-        }
-        },1000)
-    }
-
-    async getApi(){
-        fetch('../page_config.json')
-            .then((req)=>req.json())
-            .then((res)=>{
-                this.data=Object.assign({},res)
-                return this.data
-            })
-            .catch((error)=>(console.error(error)))
-            .finally(await console.log("ajo"));
-    }
-}*/
 
 class Datos{
-    static datos = {}; 
-    /*onstructor(){
-        
-        this.getApi();
-    }*/
+
     static getData(){
-        this.getApi()
-        return this.datos;
-    }
-    //Metodo
-    static async getApi(){
-        fetch("/page_config.json")
-            .then(req => req.json())
-            .then((res) =>{
-                for(let i in res){
-                    this.datos[i]=res[i];
-                }
-                return this.datos;
-            })
+        let txtReader= new XMLHttpRequest()
+
+        txtReader.open('GET','../page_config.json',false)
+        txtReader.send(false)
+
+        this.json = JSON.parse(txtReader.responseText)
+
+        return this.json;
     }
 }
+
 
 export {Datos}
--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,103 @@
+//Obtención de datos base
 import { Datos } from "./backend.js"
+const DATA = Datos.getData();
 
 const header = document.createElement('header')
 const mainNav = document.createElement('nav')
 const contextNav = document.createElement('nav')
+const footer = document.createElement('footer')
 
-const data = Datos.getData();
+const main = document.querySelector('main')
 
+console.log(DATA)
+
+const setMeta = () =>{
+    const head = document.querySelector('head')
+
+    head.querySelector('title').textContent = DATA.game_name
+
+    const meta = document.createElement('meta')
+
+    for(let metadata of DATA.metadata){
+        const metaClone = meta.cloneNode(true)
+        for(let m in metadata ){
+            console.log(m)
+            metaClone.setAttribute(m,metadata[m])
+        }
+        head.appendChild(metaClone)
+    }
+
+    const link = document.createElement('link')
+
+    for(let lin of DATA.links){
+        const linkClone = link.cloneNode(true)
+        for(let l in lin ){
+            linkClone.setAttribute(l,lin[l])
+        }
+        head.appendChild(linkClone)
+    }
+}
+
+const setMainNav = () => {
+    const a = document.createElement('a')
+
+    console.log(window.location.pathname)
+
+    for(let n of DATA.main_menu){
+        const aClone = a.cloneNode(true)
+        aClone.textContent = n.view_title
+        if(n.view_route==window.location.pathname){ 
+            aClone.classList.toggle('active_view')
+        }
+        else {
+            aClone.classList.toggle('available_view')
+            aClone.setAttribute('href',n.view_route)
+        }
+        
+        mainNav.appendChild(aClone)
+    }
+    /*
+    mainNav.addEventListener("contextmenu",(event)=>{
+        event.preventDefault();
+    }, false);
+
+    mainNav.addEventListener("copy", function(event){
+        // Change the copied text if you want
+        event.clipboardData.setData("text/plain", "No se permite copiar en esta página web");
+        
+        // Prevent the default copy action
+        event.preventDefault();
+    }, false);*/
+
+}
+
+const setHeader = () =>{
+    setMainNav()
+
+    const img = document.createElement('img')
+    img.setAttribute('src',DATA.game_logo)
+    img.setAttribute('alt','game_logo')
+    header.appendChild(img)
+    header.appendChild(mainNav)
+    main.before(header)
+}
+
+const setFooter = () =>{
+    const license = document.createElement('span')
+    license.textContent = 'Se me olvido que se escribe xd'
+    footer.appendChild(license)
+    main.after(footer)
+}
+
+const setMain = () => {
+    document.body.querySelector('main').querySelector('section').classList.add('first-section')
+}
+
+const renderTemplate = () =>{
+    setMeta()
+    setHeader()
+    setMain()
+    setFooter()
+}
+
+renderTemplate()
